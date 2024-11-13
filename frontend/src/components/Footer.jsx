cb6f45c8574edf2e325d92168f02ad481f2bdb21
Ariakash01 <ariakash067@gmail.com>
2024-11-13 15:30:55 +0530

    first commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,6 +7,13 @@ import '../App.css'
 import {LiaInstagram} from 'react-icons/lia'
 import {LiaFacebook} from 'react-icons/lia'
 import {CiTwitter} from 'react-icons/ci'
+import { BiLogoGmail } from "react-icons/bi";
+import { BsBrowserChrome } from "react-icons/bs";
+import { FaInstagram } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { GrInstagram } from "react-icons/gr";
+import { FaYoutube } from "react-icons/fa";
+
 const Footer = () => {
   return (
 <footer id="footer">
@@ -17,9 +24,14 @@ const Footer = () => {
     <p>  Developed by Ariakash S Information Technology Dr.Sacoe (2021-25).All rights reserved</p>
   </div>
   <div className="ftSocials">
-  <li><a href="https://www.facebook.com/"><LiaFacebook/></a></li>
-  <li><a href="https://www.instagram.com/i_am_akash._._/"><LiaInstagram/></a></li>
-  <li><a href="https://www.twitter.com/"><CiTwitter/></a></li>
+  <li><a href="mailto:ariakash067@gmail.com"><BiLogoGmail /></a></li>
+
+
+  <li><a href="https://erp.aei.edu.in/"><BsBrowserChrome /></a></li>
+
+
+  <li><a href="https://www.youtube.com/@drsacoeofficial1886"><FaYoutube /></a></li>
+  <li><a href="https://x.com/drsacoe?lang=en"><FaXTwitter /></a></li>
   </div>
 </footer>
   )
